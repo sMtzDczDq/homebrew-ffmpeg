baff8cb32d196a1edd5c7c3f28d376db32a80134
sMtzDczDq <75165692+sMtzDczDq@users.noreply.github.com>
2026-08-28 07:27:22 +0200

    Update ffmpeg-full.rb

---
+++ Formula/ffmpeg-full.rb
@@ -0,0 +1,184 @@
+class FfmpegFull < Formula
+  desc "Play, record, convert, and stream many audio and video codecs"
+  homepage "https://ffmpeg.org/"
+  url "https://ffmpeg.org/releases/ffmpeg-9.0.1.tar.xz"
+  sha256 "cf38e0e28c7e5605942c4a77755349b0145804a397af37eb1fb4c77cb237f635"
+  # None of these parts are used by default, you have to explicitly pass `--enable-gpl`
+  # to configure to activate them. In this case, FFmpeg's license changes to GPL v2+.
+  # Passing `--enable-version3` changes the license to GPL v3+.
+  license "GPL-3.0-or-later"
+  revision 1
+  head "https://github.com/FFmpeg/FFmpeg.git", branch: "master"
+
+  livecheck do
+    formula "ffmpeg"
+  end
+
+  bottle do
+    sha256 arm64_tahoe:   "4e281c5770f8fb5d8354ac82eec967155b95f70a49de85f2c6f63ff8b934ac7d"
+    sha256 arm64_sequoia: "84785c5d016434fdc036a7dea309e0cfef872de4bd7e86e428aa36b3cb280a72"
+    sha256 arm64_sonoma:  "a12d53f94b4e386a94b444ac5116671aed70f9fc732c779368fe1e98311beb42"
+    sha256 sonoma:        "62800379836fe9eaf3c4575bd3182ddcd9b6d5680840ddb4274994c2208d303b"
+    sha256 arm64_linux:   "7983d99392057dfa17f1fb143a3c6a672e0d663b99bc19ab7912a58f5aad3217"
+    sha256 x86_64_linux:  "b9f307fa6fde35433ae7915aea050e0a18c21117c995a447bc2519458055822d"
+  end
+
+  keg_only :versioned_formula
+
+  depends_on "pkgconf" => :build
+  depends_on "aom"
+  depends_on "aribb24"
+  depends_on "dav1d"
+  depends_on "fontconfig"
+  depends_on "freetype"
+  depends_on "frei0r" => :no_linkage
+  depends_on "ggml"
+  depends_on "gnutls"
+  depends_on "harfbuzz"
+  depends_on "jpeg-xl"
+  depends_on "lame"
+  depends_on "libass"
+  depends_on "libbluray"
+  depends_on "libplacebo"
+  depends_on "librist"
+  depends_on "libsoxr"
+  depends_on "libssh"
+  depends_on "libvidstab"
+  depends_on "libvmaf"
+  depends_on "libvorbis"
+  depends_on "libvpx"
+  depends_on "libx11"
+  depends_on "libxcb"
+  depends_on "opencore-amr"
+  depends_on "openjpeg"
+  depends_on "opus"
+  depends_on "qrencode"
+  depends_on "rav1e"
+  depends_on "rubberband"
+  depends_on "sdl2-compat"
+  depends_on "snappy"
+  depends_on "speex"
+  depends_on "srt"
+  depends_on "svt-av1"
+  depends_on "tesseract"
+  depends_on "theora"
+  depends_on "webp"
+  depends_on "whisper-cpp"
+  depends_on "x264"
+  depends_on "x265"
+  depends_on "xvid"
+  depends_on "xz"
+  depends_on "zeromq"
+  depends_on "zimg"
+
+  uses_from_macos "bzip2"
+  uses_from_macos "libxml2"
+
+  on_macos do
+    depends_on "libarchive"
+    depends_on "libogg"
+    depends_on "libsamplerate"
+  end
+
+  on_linux do
+    depends_on "alsa-lib"
+    depends_on "libxext"
+    depends_on "libxv"
+    depends_on "zlib-ng-compat"
+  end
+
+  on_intel do
+    depends_on "nasm" => :build
+  end
+
+  def install
+    # The new linker leads to duplicate symbol issue https://github.com/homebrew-ffmpeg/homebrew-ffmpeg/issues/140
+    ENV.append "LDFLAGS", "-Wl,-ld_classic" if DevelopmentTools.ld64_version.between?("1015.7", "1022.1")
+
+    args = %W[
+      --prefix=#{prefix}
+      --enable-shared
+      --enable-pthreads
+      --enable-version3
+      --cc=#{ENV.cc}
+      --host-cflags=#{ENV.cflags}
+      --host-ldflags=#{ENV.ldflags}
+      --enable-ffplay
+      --enable-gnutls
+      --enable-gpl
+      --enable-libaom
+      --enable-libaribb24
+      --enable-libbluray
+      --enable-libdav1d
+      --enable-libharfbuzz
+      --enable-libjxl
+      --enable-libmp3lame
+      --enable-libopus
+      --enable-libplacebo
+      --enable-libqrencode
+      --enable-librav1e
+      --enable-librist
+      --enable-librubberband
+      --enable-libsnappy
+      --enable-libsrt
+      --enable-libssh
+      --enable-libsvtav1
+      --enable-libtesseract
+      --enable-libtheora
+      --enable-libvidstab
+      --enable-libvmaf
+      --enable-libvorbis
+      --enable-libvpx
+      --enable-libwebp
+      --enable-libx264
+      --enable-libx265
+      --enable-libxml2
+      --enable-libxvid
+      --enable-lzma
+      --enable-libfontconfig
+      --enable-libfreetype
+      --enable-frei0r
+      --enable-libass
+      --enable-libopencore-amrnb
+      --enable-libopencore-amrwb
+      --enable-libopenjpeg
+      --enable-libspeex
+      --enable-libsoxr
+      --enable-libzmq
+      --enable-libzimg
+      --enable-whisper
+      --disable-libjack
+      --disable-indev=jack
+    ]
+
+    # Needs corefoundation, coremedia, corevideo
+    args += %w[--enable-videotoolbox --enable-audiotoolbox] if OS.mac?
+    args << "--enable-neon" if Hardware::CPU.arm?
+
+    system "./configure", *args
+    system "make", "install"
+
+    # Build and install additional FFmpeg tools
+    system "make", "alltools"
+    bin.install (buildpath/"tools").children.select { |f| f.file? && f.executable? }
+    pkgshare.install buildpath/"tools/python"
+  end
+
+  def caveats
+    <<~EOS
+      ffmpeg-full includes additional tools and libraries that are not included in the regular ffmpeg formula.
+    EOS
+  end
+
+  test do
+    # Create a 5 second test MP4
+    mp4out = testpath/"video.mp4"
+    system bin/"ffmpeg", "-filter_complex", "testsrc=rate=1:duration=5", mp4out
+    assert_match(/Duration: 00:00:05\.00,.*Video: h264/m, shell_output("#{bin}/ffprobe -hide_banner #{mp4out} 2>&1"))
+
+    # Re-encode it in HEVC/Matroska
+    mkvout = testpath/"video.mkv"
+    system bin/"ffmpeg", "-i", mp4out, "-c:v", "hevc", mkvout
+    assert_match(/Duration: 00:00:05\.00,.*Video: hevc/m, shell_output("#{bin}/ffprobe -hide_banner #{mkvout} 2>&1"))
+  end
+end
